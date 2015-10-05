30ba93254bcc9d2ed2df378b9074805e164acc7d
dot.Gen <vglez.dotgen@gmail.com>
2015-10-06 00:48:06 +0100

    Update upload.php
    Añadiendo extensión de archivos (jpg y mp3) cuando se crea el archivo en el servidor

--- botonSocial (Servidor)/botonsocial/upload.php
+++ botonSocial (Servidor)/botonsocial/upload.php
@@ -1,5 +1,8 @@
 <?php
     
+    $ext_img=".jpg";
+    $ext_audio=".mp3";
+    
     $target_dir_img = "uploads/img/";
     $target_dir_audio = "uploads/audio/";
 
@@ -15,7 +18,7 @@
     if (isset($_FILES['photo'])) {
         $temp = explode(".", $_FILES["photo"]["name"]);
         $filename_img = round(microtime(true)) . '.' . end($temp);
-        move_uploaded_file($_FILES["photo"]["tmp_name"], $target_dir_img.$filename_img);
+        move_uploaded_file($_FILES["photo"]["tmp_name"], $target_dir_img.$filename_img.$ext_img);
     }
 
     /*Store Audio*/
@@ -25,7 +28,7 @@
     if (isset($_FILES['audio'])) {
         $temp = explode(".", $_FILES["audio"]["name"]);
         $filename_audio = round(microtime(true)) . '.' . end($temp);
-        move_uploaded_file($_FILES["audio"]["tmp_name"], $target_dir_audio.$filename_audio);
+        move_uploaded_file($_FILES["audio"]["tmp_name"], $target_dir_audio.$filename_audio.$ext_audio);
     }
 
 
